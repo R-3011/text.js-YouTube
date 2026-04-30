14387ba8a7b6e0f86f175bb5e42b3c2fee63ba38
R-3011 <rekhaodedara383@gmail.com>
2026-04-30 15:38:48 +0000

    conversion of dataTypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,16 @@ let accountEmail = "rekha@google.com"
 var accountPassword = "12345"
 accountCity = "jaipur"
 
-accountId = 2
+//accountId = 2
 
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "banglore"
+let accountState;
 console.log(accountId);
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity ,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,6 @@
+"use strict";//treat all js code as newer version
+
+
+//object
+
+console.log(typeof "Rekha");
